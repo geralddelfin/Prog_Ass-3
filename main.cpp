2a42dcd1d8f262797e1ddef2c4e5fc0bdf3accc5
Gerald Delfin <42112978+geralddelfin@users.noreply.github.com>
2018-08-27 01:01:32 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+      int one, two, three, four;
+      cout<<"\n\n--------------Can you help Johnny?--------------\n\n";
+      cout<<"\n\n If Johnny had 4 different types of pond fish, how many would it be in total?\n\n";
+      cout<< "Enter four numbers to calculate how many there are:\n";
+      cout<< "Enter first goldfish count:\n";
+      cin>>one;
+      cout<< "Enter second janitor fish count:\n";
+      cin>>two;
+      cout<< "Enter third sunfish count:\n";
+      cin>>three;
+      cout<< "Enter fourth sturgeon fish count:\n";
+      cin>>four;
+      cout<<"\n\n";
+      cout<< "Currently calculating Johnny's total...\n\n";
+      cout<< "Johnny's total count is = ";
+      cout<< one+two+three+four;
+      cout<< "\n--------------Thanks for helping Johnny--------------";
+      cout<<"\n\n";
+
+      return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
